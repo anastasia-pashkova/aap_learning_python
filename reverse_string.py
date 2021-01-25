e8991a65c897dca2674c8c25ec57155d163fb366
anastasia-pashkova <anastasia-pashkova@github.com>
2021-01-25 12:09:01 -0800

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+
+        if not s:
+            return
+        
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            temp_val = s[left]
+            s[left] = s[right]
+            s[right] = temp_val
+            left +=1
+            right -=1
+            
+            
